b3ef82c42109293f3d08ecdcfef11968e7e8a717
hbc <bcxxxxxx@gmail.com>
2018-05-23 13:50:31 +0800

    feat: display stream message

--- cmd/pbmon/main.go
+++ cmd/pbmon/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -14,7 +15,9 @@ import (
 )
 
 var (
-	httpAddr string
+	httpAddr      string
+	pbFile        string
+	pbMessageType string
 )
 
 var (
@@ -23,6 +26,8 @@ var (
 
 func main() {
 	flag.StringVar(&httpAddr, "httpAddr", ":12223", "http listen address")
+	flag.StringVar(&pbFile, "pbFile", "pb.proto", "protobuf file")
+	flag.StringVar(&pbMessageType, "pbMessageType", "Envelope", "protobuf root messge type")
 
 	flag.Parse()
 
@@ -30,16 +35,33 @@ func main() {
 		httpAddr = fmt.Sprintf(":%s", port)
 	}
 
+	if file := os.Getenv("PBFILE"); file != "" {
+		pbFile = file
+	}
+
+	if mt := os.Getenv("PBMESSAGE"); mt != "" {
+		pbMessageType = mt
+	}
+
 	setupLogger()
-	startServer(httpAddr)
+	pb := mustReadPbFile()
+	startServer(httpAddr, pb, pbMessageType)
 }
 
 func setupLogger() {
 	log.SetFlags(log.LstdFlags)
 }
 
-func startServer(httpAddr string) {
-	server := NewServer(httpAddr)
+func mustReadPbFile() string {
+	c, err := ioutil.ReadFile(pbFile)
+	if err != nil {
+		panic(err)
+	}
+	return string(c)
+}
+
+func startServer(httpAddr, pb, pbMessageType string) {
+	server := NewServer(httpAddr, pb, pbMessageType)
 
 	server.Start()
 }
@@ -49,13 +71,19 @@ type Server struct {
 	httpServer *http.Server
 	upgrader   *websocket.Upgrader
 	stream     *stream.Stream
+
+	pb            string
+	pbMessageType string
 }
 
-func NewServer(httpAddr string) *Server {
+func NewServer(httpAddr, pb, pbMessageType string) *Server {
 	server := &Server{
 		httpMux:  mux.NewRouter(),
 		upgrader: &websocket.Upgrader{},
 		stream:   stream.NewMemStream(),
+
+		pb:            pb,
+		pbMessageType: pbMessageType,
 	}
 
 	server.httpServer = &http.Server{
@@ -64,6 +92,8 @@ func NewServer(httpAddr string) *Server {
 	}
 
 	server.httpMux.HandleFunc("/stream", server.HandleStream).Methods("GET")
+	server.httpMux.HandleFunc("/proto", server.HandleProto).Methods("GET")
+	server.httpMux.HandleFunc("/proto/settings", server.HandleProtoSettings).Methods("GET")
 	server.httpMux.HandleFunc("/", server.HandleIndex).Methods("GET")
 
 	return server
@@ -89,6 +119,8 @@ func (s Server) HandleStream(w http.ResponseWriter, r *http.Request) {
 	}
 	defer reader.Close()
 
+	c.WriteMessage(websocket.BinaryMessage, []byte("hello"))
+
 	buf := make([]byte, 32*1024)
 	for {
 		_, err := reader.Read(buf)
@@ -103,6 +135,16 @@ func (s Server) HandleStream(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+}
+
+func (s Server) HandleProto(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/vnd.google.protobuf")
+	w.Write([]byte(s.pb))
+}
+
+func (s Server) HandleProtoSettings(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(fmt.Sprintf(`{"message_type": "%s"}`, s.pbMessageType)))
 }
 
 func (s Server) HandleIndex(w http.ResponseWriter, r *http.Request) {
